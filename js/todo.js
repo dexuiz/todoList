3160123c0465e8ed072c5bbf4092dd13d3b1466d
dexuiz <dex345z@gmail.com>
2017-01-17 20:26:49 +0530

    complete

--- js/todo.js
+++ js/todo.js
@@ -3,15 +3,19 @@ $('ul').on("click","li",function(){
   $(this).toggleClass("complete");
 });
 
-$('#enter').click(function(){
-  var listItem = $('input').val();
-  $("ul").append("<li>"+listItem+"</li>");
-  $("input").val("");
+$('input').keypress(function(event){
+  if(event.which == 13){
+    var listItem = $('input').val();
+    $("ul").append("<li>"+listItem+"</li>");
+    $("input").val("");
+  }
 });
-
-
 
 $('ul').on("dblclick","li",function(){
   console.log("remove object");
   $(this).remove();
+})
+
+$('i').click(function(){
+  $('input').slideToggle(100);
 })
